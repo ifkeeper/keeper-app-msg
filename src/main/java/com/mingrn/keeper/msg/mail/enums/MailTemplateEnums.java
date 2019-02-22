aa68114fd4e7dbcde7a8752002ee54bb1b6e14c3
MinGRn <MinGRn97@gmail.com>
2019-02-22 18:06:37 +0800

    增加:枚举参数校验邮件模板;

--- src/main/java/com/mingrn/keeper/msg/mail/enums/MailTemplateEnums.java
+++ src/main/java/com/mingrn/keeper/msg/mail/enums/MailTemplateEnums.java
@@ -1,5 +1,11 @@
 package com.mingrn.keeper.msg.mail.enums;
 
+import com.mingrn.keeper.global.annotation.Checked;
+import com.mingrn.keeper.msg.util.FreemarkerUtil;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
 /**
  * 模板枚举类
  *
@@ -7,4 +13,23 @@ package com.mingrn.keeper.msg.mail.enums;
  * @date 2019-02-19 21:36
  */
 public enum MailTemplateEnums {
+    /**
+     * 账号激活验证
+     */
+    REGISTER_VERIFY("register-verify.ftl");
+
+    private String template;
+
+    MailTemplateEnums(String template) {
+        this.template = template;
+    }
+
+    @Checked
+    public static String genTemplateHtml(@NotNull MailTemplateEnums templateEnums, Map<String, Object> data) {
+        return FreemarkerUtil.genFtl2String(templateEnums.getTemplate(), data);
+    }
+
+    public String getTemplate() {
+        return template;
+    }
 }
